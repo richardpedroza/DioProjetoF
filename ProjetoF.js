48dd0080d31b117aafa89c97e09b97fe24397de7
Piratewar <richardlessa1@hotmail.com>
2024-11-27 11:23:02 -0300

    Batalha Finalizada

--- ProjetoF.js
+++ ProjetoF.js
@@ -1,1 +1,44 @@
-console.log ("Ola mundo")
+const heroi = "SrDavos"
+let = xpTotal = 0
+for (i = 0; i < 15; i++){
+    let xpMonstro = Math.floor(Math.random() * 1000); 
+    let nivelMonstro = Math.floor(Math.random() * 100);
+    // calculo
+    let xpAdquirido = (xpMonstro * nivelMonstro) / 2;
+    xpTotal += xpAdquirido;
+    //switch
+    let rank;
+    switch (true) {
+        case xpTotal < 1000:
+            rank = "Ferro";
+            break;
+        case xpTotal <= 2000:
+            rank = "Bronze";
+            break;
+        case xpTotal <= 5000:
+            rank = "Prata";
+            break;
+        case xpTotal <= 7000:
+            rank = "Ouro";
+            break;
+        case xpTotal <= 8000:
+            rank = "Platina";
+            break;
+        case xpTotal <= 9000:
+            rank = "Ascendente";
+            break;
+        case xpTotal <= 10000:
+            rank = "Imortal";
+            break;
+        default:
+            rank = "Radiante";
+            break;}
+
+//console
+console.log ("batalha " + (i + 1) + ":")
+console.log ("XP do monstro " + nivelMonstro)
+console.log ("Nivel do Monstro " + nivelMonstro)
+console.log (heroi + " adquiriu " + xpAdquirido)
+console.log ("XP total Acumulado " + faixa + "\n")
+console.log ("Apos varias batalhas, " + heroi + " acumulou " + xpTotal + " Experiencia assim chegando ao Rank " + rank);
+    }
